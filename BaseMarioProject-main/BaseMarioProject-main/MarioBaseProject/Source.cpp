a57c5548ee8ab5c26693271ad121680d944e2384
Joshua Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-03-14 10:49:01 +0000

    tutorial 2 completed with annoying errors

--- BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Source.cpp
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -3,10 +3,13 @@
 #include <SDL_mixer.h>
 #include "constants.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 SDL_Window* g_window = nullptr;
+SDL_Renderer* g_renderer = nullptr;
+SDL_Texture* g_texture = nullptr;
 
 int main(int argc, char* args[])
 {
@@ -16,6 +19,7 @@ int main(int argc, char* args[])
 
 		while (!quit)
 		{
+			Render();
 			quit = Update();
 		}
 	}
@@ -50,7 +54,81 @@ bool InitSDL()
 		}
 		return true;
 	}
+	g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+	if (g_renderer != nullptr)
+	{
+		//init PNG loading
+		int imageFlags = IMG_INIT_PNG;
+		if (!(IMG_Init(imageFlags) & imageFlags))
+		{
+			cout << "SDL_Image could not initialise. Error: " << IMG_GetError();
+			return false;
+		}
+	}
+	else
+	{
+		cout << "Renderer could not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	
+	g_texture = LoadTextureFromFile("Images/test.bmp");
+	if (g_texture == nullptr)
+	{
+		return false;
+	}
 }
+
+void Render()
+{
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+
+	//where to render
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+
+	//Render to screen
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+
+	//update
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	//remove memory from previous texture
+	FreeTexture();
+
+	SDL_Texture* p_texture = nullptr;
+
+	//load image
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_texture == nullptr)
+		{
+			cout << "Unable to create texture from surface. Error: " << SDL_GetError();
+		}
+		//remove the loaded surface now that we have a texture
+		SDL_FreeSurface(p_surface);
+	}
+	else
+	{
+		cout << "Unable to create texture from surface. Error: " << IMG_GetError();
+	}
+
+	return p_texture;
+}
+
+void FreeTexture()
+{
+	if (g_texture != nullptr)
+	{
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
+}
+
 void CloseSDL()
 {
 	SDL_DestroyWindow(g_window);
@@ -58,6 +136,10 @@ void CloseSDL()
 
 	IMG_Quit();
 	SDL_Quit();
+
+	FreeTexture();
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
 }
 
 bool Update()
--- BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/constants.h
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/constants.h
@@ -2,7 +2,12 @@
 //screen Dimensions
 #define SCREEN_WIDTH 512
 #define SCREEN_HEIGHT 416
+#include <string>
 
 bool InitSDL();
 void CloseSDL();
 bool Update();
+
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+void FreeTexture();
