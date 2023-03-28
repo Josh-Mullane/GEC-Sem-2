dbd394871dcebb3f836c42343bd305b058a9d0c7
Joshua Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-03-28 10:50:34 +0100

    refactoring

---
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Texture2D.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SDL.h>
+#include <string>
+#include "commons.h"
+using namespace std;
+#ifndef _TEXTURE2D_H
+#define _TEXTURE2D_H
+class Texture2D
+{
+public:
+
+private:
+
+};
+#endif //_TEXTURE2D_H
+
