c9b1bd0e04b3f1b6ffc1eea6de6ab5305294302d
Josh-Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-04-14 06:39:09 +0100

    cont.,

--- BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Texture2D.cpp
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Texture2D.cpp
@@ -1,1 +1,11 @@
 #include "Texture2D.h"
+#include <SDL_image.h>
+#include <iostream>
+
+using namespace std;
+
+Texture2d::Texture2D(SDL_Renderer* renderer);
+
+bool LoadFromFile(std::string path);
+void Free();
+void Render(Vector2D new_position, SDL_RendererFlip flip, double angle 0.0);
