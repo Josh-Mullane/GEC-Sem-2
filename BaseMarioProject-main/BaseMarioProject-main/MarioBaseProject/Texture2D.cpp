963649c764be6d3279bde31c9a509b55f97239d6
Joshua Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-05-02 10:17:49 +0100

    progress lost?

--- BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Texture2D.cpp
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Texture2D.cpp
@@ -4,8 +4,47 @@
 
 using namespace std;
 
-Texture2D::Texture2D(SDL_Renderer* renderer);
+Texture2D::Texture2D(SDL_Renderer* renderer)
+{
+	m_renderer = renderer;
 
-bool LoadFromFile(std::string path);
-void Free();
-void Render(Vector2D new_position, SDL_RendererFlip flip, double angle = 0.0);
+}
+
+Texture2D::~Texture2D()
+{
+	Free();
+
+	m_renderer = nullptr;
+}
+
+
+
+bool Texture2D::LoadFromFile(std::string path)
+{
+	SDL_Texture* m_texture = nullptr;
+	SDL_Surface* m_surface = IMG_Load(path.c_str());
+	if (m_surface != nullptr)
+	{
+
+	}
+
+}
+
+void Texture2D::Render(Vector2D new_position, SDL_RendererFlip flip, double angle)
+{
+	SDL_Rect renderLocation = { 0,0,m_width, m_height };
+
+	SDL_RenderCopyEx(m_renderer, m_texture, nullptr, &renderLocation, 0, nullptr, SDL_FLIP_NONE);
+}
+
+
+void Texture2D::Free()
+{
+	if (m_texture != nullptr)
+	{
+		SDL_DestroyTexture(m_texture);
+		m_texture = nullptr;
+		m_width = 0;
+		m_height = 0;
+	}
+}
