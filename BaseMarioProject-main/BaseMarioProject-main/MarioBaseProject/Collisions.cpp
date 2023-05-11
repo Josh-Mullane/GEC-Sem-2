8e2af6cf02b77d75d434ebd19441d74284db9995
Josh-Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-05-11 15:03:00 +0100

    week 5

--- BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Collisions.cpp
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Collisions.cpp
@@ -3,7 +3,27 @@
 
 Collisions* Collisions::m_instance = nullptr;
 
-Collisions* Instance()
+
+Collisions::Collisions()
 {
-	if(!m_instance)
+
+}
+
+Collisions::~Collisions()
+{
+	m_instance = nullptr;
+}
+
+Collisions* Collisions::Instance()
+{
+	if (!m_instance)
+	{
+		m_instance = new Collisions;
+	}
+	return m_instance;
+}
+
+bool Collisions::Circle(Character* character1, Character* character2)
+{
+	
 }
